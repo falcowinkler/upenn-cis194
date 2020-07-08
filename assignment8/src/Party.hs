6a7ea1b039b1f2464f2b794642a7c1e2e3d20d01
falco.winkler <falco.winkler@thoughtworks.com>
2020-07-08 10:43:49 +0200

    sort of finished assignment 8 ~

--- assignment8/src/Party.hs
+++ assignment8/src/Party.hs
@@ -7,7 +7,7 @@ glCons emp (GL employees fun) = (GL (emp:employees) (fun + (empFun emp)))
 
 
 instance Semigroup GuestList where
-  (GL emps1 fun1) <> (GL emps2 fun2) = (GL (emps1 ++ emps2) (fun1 + fun2))
+  (<>) = moreFun
 
 instance Monoid GuestList where
   mempty = GL [] 0
@@ -21,8 +21,27 @@ moreFun gl1@(GL emp1 fun1) gl2@(GL emp2 fun2)
 -- regarding Exercise 2
 -- trees are actuall foldable
 
+-- folding over a tree of arbitrary children count requires
+-- the subtree [b] as agument in f
+treeFold :: (a -> [b] -> b) -> b -> Tree a -> b
+treeFold f b (Node l forest) = f l (map (treeFold f b) forest)
 
-treeFold :: (a -> b -> b) -> b -> Tree a -> b
-treeFold f b (Node l forest) = foldr (\tree  otherb -> treeFold f otherb tree) newB forest
+
+nextLevel :: Employee -> [(GuestList, GuestList)] -> (GuestList, GuestList)
+nextLevel bob subtrees = ((glCons bob bestSubresult), bestSubresult)
   where
-    newB = (f l b)
+    (bestSubtreeWithBoss, bestSubtreeWithoutBoss) = mconcat subtrees
+    bestSubresult = moreFun bestSubtreeWithBoss bestSubtreeWithoutBoss
+
+maxFun :: Tree Employee -> GuestList
+maxFun tree = moreFun withCEO withoutCEO
+  where
+    (withCEO, withoutCEO) = treeFold nextLevel (mempty, mempty) tree
+
+-- bit unclear what is expected in the task
+-- people right under the boss have zero fun,
+-- but `glsCons` should just add the fun score when appending?
+party :: IO ()
+party = do
+  company <- readFile "./company.txt"
+  putStrLn (show $ maxFun $ read company)
--- assignment8/test/PartySpec.hs
+++ assignment8/test/PartySpec.hs
@@ -3,9 +3,9 @@ module PartySpec where
 
 import Party
 import Test.Hspec
-
+import Employee
 spec :: Spec
 spec = do
   describe "append join lists" $ do
     it "works" $ do
-      1 `shouldBe` 1
+      maxFun testCompany
